286a9e9adfb510ed8a07910aed76ea5ef58cac40
Niek Flipse <npaflips@avans.nl>
2020-06-22 15:31:09 +0200

    Created event product updated

---
+++ SupplierService/src/events/ProductUpdated.js
@@ -0,0 +1,10 @@
+class SupplierProductUpdated {
+    constructor(id, title, price, category) {
+        this.productId = id
+        this.title = title
+        this.price = price
+        this.category = category
+    }
+}
+
+module.exports = SupplierProductUpdated
