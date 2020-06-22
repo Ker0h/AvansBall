9f86f589a7d505e320b42d702d7e1d51a5bff764
Niek Flipse <npaflips@avans.nl>
2020-06-22 16:42:35 +0200

    little cleanup

--- SupplierService/src/dataAccess/ProductReadRepository.js
+++ SupplierService/src/dataAccess/ProductReadRepository.js
@@ -25,22 +25,21 @@ class ProductReadRepository {
 
         newProduct.save()
             .then(() => {
-                console.log("Product saved in read db")
+                console.log(" [+] Product saved in read db")
             })
             .catch((e) => {
                 console.log(e)
-                console.log("Oeps het ging fout bij het opslaan in de read db")
+                console.log(" [-] Oeps het ging fout bij het opslaan in de read db")
             })
     }
 
     static updateProduct(product) {
-        console.log(product)
-        ProductRead.findOneAndUpdate({ productId: product._id }, {
+        ProductRead.findOneAndUpdate({ productId: product.productId }, {
             title: product.title, category: product.category, price: product.price
         }).then(() => {
-            console.log("Product updated in read db")
+            console.log(" [+]Product updated in read db")
         }).catch(() => {
-            console.log("oeps toch niet")
+            console.log(" [-] Oeps het ging fout bij het updaten in de read db")
         })
     }
 
